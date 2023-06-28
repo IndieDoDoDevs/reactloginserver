189051daf9468731c13ebb3ce65b7b0a7d20f286
NiroshKumarR <niroshkumar072@gmail.com>
2023-06-28 23:47:06 +0530

    main files

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Delete, Get, Inject, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { LoginService } from './login.service';
 
 @Controller('login')
@@ -6,7 +14,9 @@ export class LoginController {
   constructor(@Inject(LoginService) private loginModel: LoginService) {}
 
   @Post()
-  public async getLogin() {
+  public async getLogin(
+    @Body() data: { name: string; email: string; password: string },
+  ) {
     return await this.loginModel.loginSevice();
   }
 }
